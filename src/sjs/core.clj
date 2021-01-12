226e06d4cd40c6d8fa1e09fcfdaacd0420656699
ParkJuhyeong <majecty@gmail.com>
2021-01-12 19:50:08 +0900

    Handle error in main

--- src/sjs/core.clj
+++ src/sjs/core.clj
@@ -22,16 +22,24 @@
         new-name (str base ".js")]
     new-name))
 
-;; FIXME: handle file not found
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (f/attempt-all
-    [file-name (first args)
-     new-file-name (make-output-file-name file-name)
-     file-contents (slurp file-name)
-     sexps (log/spyf "srcs %s"
-                     (reader/read-file file-contents))
-     jssrcs (map transpiler/transpile sexps)
-     jssrc (str (str/join "\n\n" jssrcs) "\n")]
-    (spit new-file-name jssrc)))
+   [file-name (first args)
+    new-file-name (make-output-file-name file-name)
+    file-contents (try
+                    (slurp file-name)
+                    (catch Exception e
+                      (log/trace e)
+                      (f/fail "Cant't read file %s" file-name)))
+    sexps (log/spyf "srcs %s"
+                    (reader/read-file file-contents))
+    jssrcs (map transpiler/transpile sexps)
+    jssrc (str (str/join "\n\n" jssrcs) "\n")]
+
+   (spit new-file-name jssrc)
+
+   (f/when-failed
+    [e]
+    (log/error (f/message e)))))
