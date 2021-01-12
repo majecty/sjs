7e90efd897c3b11b478d7007011238ae136a30f7
ParkJuhyeong <majecty@gmail.com>
2021-01-12 18:59:36 +0900

    Use failjure

--- src/sjs/core.clj
+++ src/sjs/core.clj
@@ -4,7 +4,8 @@
             [sjs.transpiler :as transpiler]
             [clojure.string :as str]
             [clojure.test :refer [is deftest]]
-            [clojure.tools.logging :as log]))
+            [clojure.tools.logging :as log]
+            [failjure.core :as f]))
 
 (defn split-ext [file-name]
   (let [[_whole wo-ext ext] (re-matches #"(.+)\.([a-zA-z0-9]+)$" file-name)]
@@ -25,11 +26,12 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (let [file-name (first args)
-        new-file-name (make-output-file-name file-name)
-        file-contents (slurp file-name)
-        sexps (log/spyf "srcs %s"
-                        (reader/read-file file-contents))
-        jssrcs (map transpiler/transpile sexps)
-        jssrc (str (str/join "\n\n" jssrcs) "\n")]
+  (f/attempt-all
+    [file-name (first args)
+     new-file-name (make-output-file-name file-name)
+     file-contents (slurp file-name)
+     sexps (log/spyf "srcs %s"
+                     (reader/read-file file-contents))
+     jssrcs (map transpiler/transpile sexps)
+     jssrc (str (str/join "\n\n" jssrcs) "\n")]
     (spit new-file-name jssrc)))
